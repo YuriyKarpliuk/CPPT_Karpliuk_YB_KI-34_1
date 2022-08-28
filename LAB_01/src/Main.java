173be3e26558c44a33b27cf369becdc607dd86ed
Yuriy Karpliuk <ukar19062005@gmail.com>
2022-08-29 00:30:03 +0300

    LAB_01 first commit.

---
+++ LAB_01/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = new String();
+        message = "Yurii Karpliuk KI-34";
+        System.out.println(message);
+    }
+}
